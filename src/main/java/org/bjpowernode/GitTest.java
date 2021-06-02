9d626af6ce9237506e3cfaeb7dd74ea538a69373
lzy <lzy123@qq.com>
2021-06-02 12:32:48 +0800

    second commit

--- src/main/java/org/bjpowernode/GitTest.java
+++ src/main/java/org/bjpowernode/GitTest.java
@@ -3,5 +3,6 @@ package org.bjpowernode;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello GIt");
+        System.out.println("hello GIt");
     }
 }
