406efdcfc91a478edffe19683ee6d9d59481e6ef
lzy <lzy123@qq.com>
2021-06-02 19:00:03 +0800

    hot-fix commit2

--- src/main/java/org/bjpowernode/GitTest.java
+++ src/main/java/org/bjpowernode/GitTest.java
@@ -4,6 +4,8 @@ public class GitTest {
 
     public static void main(String[] args) {
 
+        System.out.println("hot-fix test");
+
         System.out.println("hello GIt");
         System.out.println("hello GIt");
         System.out.println("hello GIt");
