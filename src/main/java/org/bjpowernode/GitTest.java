6b060f363c1e0f58cf0acc45c28ea3368deb8efc
lzy <lzy123@qq.com>
2021-06-02 19:06:15 +0800

    fix-hot commit3

--- src/main/java/org/bjpowernode/GitTest.java
+++ src/main/java/org/bjpowernode/GitTest.java
@@ -9,7 +9,6 @@ public class GitTest {
         System.out.println("hello GIt");
         System.out.println("hello GIt");
         System.out.println("hello GIt");
-        System.out.println("hello GIt");
-        System.out.println("hello GIt");
+
     }
 }
