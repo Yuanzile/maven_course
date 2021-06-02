5be8ed21cf6dca11c1f3470248b2a19ce19bed10
lzy <lzy123@qq.com>
2021-06-02 19:00:54 +0800

    master commit

--- src/main/java/org/bjpowernode/GitTest.java
+++ src/main/java/org/bjpowernode/GitTest.java
@@ -10,5 +10,7 @@ public class GitTest {
         System.out.println("hello GIt");
         System.out.println("hello GIt");
         System.out.println("hello GIt");
+
+        System.out.println("master test");
     }
 }
