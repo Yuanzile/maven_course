059d31d849960d03bae7fd5e4ac0db53b5ebbe63
Yuanzile <73116580+Yuanzile@users.noreply.github.com>
2021-06-02 19:29:10 +0800

    pull commit

--- src/main/java/org/bjpowernode/GitTest.java
+++ src/main/java/org/bjpowernode/GitTest.java
@@ -13,6 +13,7 @@ public class GitTest {
         System.out.println("master test");
         System.out.println("hello GIt");
         System.out.println("push test");
+        System.out.println("pull test");
 
     }
 }
